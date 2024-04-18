4a29895998d215e5e41ad2535d178d86f28ac843
Nicolae Mogage <mogagenicolae@yahoo.com>
2024-04-18 09:01:19 +0300

    Added lab3

--- 01_Tasks/src/test/java/tasks/model/TasksOperationsTest.java
+++ 01_Tasks/src/test/java/tasks/model/TasksOperationsTest.java
@@ -97,20 +97,18 @@ public class TasksOperationsTest {
         var tasksList = new ArrayTaskList();
         var task1= new Task("Task 1", new Date(2000), new Date(4000), 2);
         task1.setActive(true);
-        var task2= new Task("Task 2", new Date(8000), new Date(10000), 2);
+        var task2= new Task("Task 2",  new Date(11000), new Date(13000), 2);
         task2.setActive(true);
         var task3 = new Task("Task 3", new Date(15000), new Date(17000), 2);
         task3.setActive(true);
-        var task4 = new Task("Task 4", new Date(11000), new Date(13000), 2);
-        task4.setActive(true);
 
         tasksList.add(task1);
         tasksList.add(task2);
         tasksList.add(task3);
-        tasksList.add(task4);
 
         TasksOperations tasksOperations = new TasksOperations(getObservableList(tasksList));
         Date startDate = new Date(1000);
+        System.out.println(startDate);
         Date endDate = new Date(startDate.getTime() + 10000);
 
         // Act
@@ -123,8 +121,6 @@ public class TasksOperationsTest {
         assertEquals("Task 1", iterator.next().getDescription());
         assertTrue(iterator.hasNext());
         assertEquals("Task 2", iterator.next().getDescription());
-        assertTrue(iterator.hasNext());
-        assertEquals("Task 4", iterator.next().getDescription());
         assertFalse(iterator.hasNext());
     }
 }
