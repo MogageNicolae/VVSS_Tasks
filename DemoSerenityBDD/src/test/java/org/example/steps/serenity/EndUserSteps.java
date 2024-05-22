3456d41fc04548cea42005ec54b50175f5af58ac
Nicolae Mogage <mogagenicolae@yahoo.com>
2024-05-22 23:31:52 +0300

    Add raport

--- DemoSerenityBDD/src/test/java/org/example/steps/serenity/EndUserSteps.java
+++ DemoSerenityBDD/src/test/java/org/example/steps/serenity/EndUserSteps.java
@@ -1,16 +1,11 @@
 package org.example.steps.serenity;
 
-import net.thucydides.core.annotations.Managed;
 import org.example.pages.CartPage;
-import org.example.pages.DictionaryPage;
 import net.thucydides.core.annotations.Step;
-import net.thucydides.core.steps.ScenarioSteps;
 import org.example.pages.LoginPage;
 import org.example.pages.ProductPage;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,35 +15,6 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
 public class EndUserSteps {
-
-//    DictionaryPage dictionaryPage;
-//
-//    @Step
-//    public void enters(String keyword) {
-//        dictionaryPage.enter_keywords(keyword);
-//    }
-//
-//    @Step
-//    public void starts_search() {
-//        dictionaryPage.lookup_terms();
-//    }
-//
-//    @Step
-//    public void should_see_definition(String definition) {
-//        assertThat(dictionaryPage.getDefinitions(), hasItem(containsString(definition)));
-//    }
-//
-//    @Step
-//    public void is_the_home_page() {
-//        dictionaryPage.open();
-//    }
-//
-//    @Step
-//    public void looks_for(String term) {
-//        enters(term);
-//        starts_search();
-//    }
-
     private LoginPage loginPage;
     private ProductPage productPage;
     private CartPage cartPage;
